01737d6bd461fee0e2f460e8cf20ffe4d0475cb9
Aldo Socarras <air.socarras@gmail.com>
2021-02-21 21:14:52 -0500

    Installed and initialized Parse SDK

---
+++ app/src/main/java/com/example/parseagram/ParseApplication.java
@@ -0,0 +1,22 @@
+package com.example.parseagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+
+    // Initializes Parse SDK as soon as the application is created
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Register parse models
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("dHp9L3P1Ak5x7vqF6fz3hUiK3SYVeRuxiwH2cbmF")
+                .clientKey("FIFQEzBltqSe4qhdYlofworLT0Xn5U0aDTfJS39w")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
